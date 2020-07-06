5eb11e1e2f6b39b9bf41e29c65b936c179f42298
Marcus Zillner <60900721+MarcusMadMax@users.noreply.github.com>
2020-07-07 00:19:14 +1200

    more changes to the acco pages and improoved javascrit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 var iNights = 1
 var iPeople = 1
 
-// var isLoginFormValid = false
-
 $(function () {
 
     //svg Mauri Hook animation-----------------------
@@ -76,7 +74,7 @@ $(function () {
         moveSignUp.play()
     })
 
-    //Inject name-------------------------------------------------------
+    //Get name from log in-------------------------------------------------------
     var logInButton = document.querySelector('.LogIn')
     logInButton.addEventListener('click', getName)
 
@@ -132,6 +130,8 @@ $(function () {
     })
 
 
+
+
     var secondShape = "M -35.73,45.41 C -9.412,61.01 -30.93,379.4 -17.31,545.8 -12.26,607.5 -54.94,740.4 6.142,730.1 63.67,720.4 26.97,284.9 27.01,202.3 27.06,104.3 51.66,29.07 106,54.36 160.3,79.65 103.7,491.7 187.7,465.7 231.9,452 156.6,99.89 249.4,94.08 285.7,91.81 299.9,127.5 305,190.9 316,327.7 328.6,462.6 321.1,598.3 315.8,695.4 294.5,776.7 353.9,773.6 415.6,770.4 379.8,650.7 368.7,588.8 337.4,415 369.6,190.1 391.1,111 412.5,31.92 457,96.83 463.3,127.2 480.9,212.1 493.9,307.8 489,396.1 487.4,425.7 482.1,460.1 517.1,455.2 548.5,450.7 476.2,166 550.9,168.9 594.8,170.6 591.6,626.8 586.3,663.5 578.4,717.8 609.1,742.4 633.9,700.6 651.9,670.2 578.3,209.7 650.8,194.1 723.2,178.5 700.8,277.9 687.6,401.1 680.2,470 766.1,486.3 756,414.3 750.3,373.5 703.1,145.6 793.4,146.1 939.9,146.8 846.2,556.8 844,601.8 841.9,646.8 878.6,682.8 903.3,630.6 928,578.4 863.2,264.8 891.7,178.8 920.2,92.81 997.3,215.6 972,292.8 946.6,370 1030,353.1 999,295.7 985.9,271.6 977.1,119.1 1048,117.8 1119,116.5 1127,634.6 1123,682.6 1119,730.6 1110,749.8 1118,771.3 1134,815.5 1173,803.5 1164,734 1155,664.5 1139,665.8 1143,418.4 1148,170.9 1225,122 1240,215 1249,273 1202,413 1282,391.7 1324,380.7 1280,165.5 1316,159.6 1362,152 1296,358.2 1379,361.2 1462,364.2 1312,753 1444,751.2 1592,749.2 1484,458.5 1505,312.2 1518,221.3 1544,58.44 1545,39.57 1562,-514.4 -480.6,-322.6 -35.73,45.41 Z";
 
     var textWrapper = document.querySelector('#section3 header h2');
@@ -157,7 +157,7 @@ $(function () {
     })
 
     slideUp.add({
-        targets: 'h2 .letter',
+        targets: 'h2',
         translateY: [-100, 0],
         opacity: [0, 1],
         easing: "easeInExpo",
@@ -167,15 +167,18 @@ $(function () {
     }, '-=6000')
 
     $('.explore').on('click', function () {
-        if(iPeople == 1 && iNights == 1){
-            $('.people').html('You added: ' + iPeople + ' person and you will stay for ' + iNights + ' night')
-        }else if (iPeople == 1 && iNights > 1){
-            $('.people').html('You added: ' + iPeople + ' person and you will stay for ' + iNights + ' nights')
-        }else if (iPeople > 1 && iNights == 1){
-            $('.people').html('You added: ' + iPeople + ' people and you will stay for ' + iNights + ' night')
-        }else {
-            $('.people').html('You added: ' + iPeople + ' people and you will stay for ' + iNights + ' nights')
+        if (iPeople == 1 && iNights == 1) {
+            $('.people').html(iPeople + ' Person ' + iNights + ' Night')
+        } else if (iPeople == 1 && iNights > 1) {
+            $('.people').html(iPeople + ' Person ' + iNights + ' Nights')
+        } else if (iPeople > 1 && iNights == 1) {
+            $('.people').html(iPeople + ' People ' + iNights + ' Night')
+        } else {
+            $('.people').html(iPeople + ' People ' + iNights + ' Nights')
         }
+        $('.nights').html(iNights)
+        $('.singlePeople').html(iPeople)
+
         slideUp.play()
 
         //show sutiable options
@@ -287,7 +290,6 @@ $(function () {
             rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`
 
             iNights = range.value
-
         }
     document.addEventListener("DOMContentLoaded", setValue);
     range.addEventListener('input', setValue);
@@ -299,6 +301,9 @@ $(function () {
     swiper.on('slideChangeTransitionEnd', function () {
         iPeople = parseInt(document.querySelector('.swiper-slide-active').innerHTML)
     })
+
+    $('span .singlePeople').html(iPeople)
+
     //People swiper caption------------------------------
     anime({
         targets: 'h6 .fa-hand-point-up',
@@ -319,18 +324,34 @@ $(function () {
     var toHostel = document.querySelector('.hostel')
     var toMotel = document.querySelector('.motel')
     var toHouse = document.querySelector('.house')
+    var backFromHotel = document.querySelector('header .hotels')
+    var backFromHostel = document.querySelector('header .hostels')
+    var backFromMotels = document.querySelector('header .motels')
+    var backFromHouses = document.querySelector('header .houses')
 
     toHotel.addEventListener('click', function () {
         document.querySelector('#section3').classList.add('toHotel')
     })
+    backFromHotel.addEventListener('click', function(){
+        document.querySelector('#section3').classList.remove('toHotel')
+    })
     toHostel.addEventListener('click', function () {
         document.querySelector('#section3').classList.add('toHostel')
+    })
+    backFromHostel.addEventListener('click', function(){
+        document.querySelector('#section3').classList.remove('toHostel')
     })
     toMotel.addEventListener('click', function () {
         document.querySelector('#section3').classList.add('toMotel')
     })
+    backFromMotels.addEventListener('click', function(){
+        document.querySelector('#section3').classList.remove('toMotel')
+    })
     toHouse.addEventListener('click', function () {
         document.querySelector('#section3').classList.add('toHouse')
+    })
+    backFromHouses.addEventListener('click', function(){
+        document.querySelector('#section3').classList.remove('toHouse')
     })
 
     //Section 3 Slide down-----------------------------------------------------
@@ -340,7 +361,7 @@ $(function () {
         duration: 200,
         autoplay: false
     })
-    $('.backTo2').on('click',function(){
+    $('.backTo2').on('click', function () {
         backTo2.play()
     })
 
@@ -393,4 +414,8 @@ $(function () {
     oForm.addEventListener('click', checkAll)
 
 
+    //Extras swipe up
+    $('.extras h3').on('click', function () {
+        $('.extras').toggleClass('swipeUp')
+    })
 })
